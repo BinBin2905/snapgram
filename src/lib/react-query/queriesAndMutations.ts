b7a72a6b9cf26f14b86910660d6cb5b68990847f
Bin <ghilavo@gmail.com>
2024-06-15 16:16:22 +0700

    Completed Explore Page

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -14,10 +14,12 @@ import {
   deletePost,
   deleteSavePosts,
   getCurrentUser,
+  getInfinitePosts,
   getPostById,
   getRecentPosts,
   likePosts,
   savePosts,
+  searchPosts,
   signInAccount,
   updatePost,
 } from "../appwrite/api";
@@ -166,5 +168,30 @@ export const useDeletePost = () => {
         queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
       });
     },
+  });
+};
+
+export const useGetPosts = () => {
+  return useInfiniteQuery({
+    queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+    queryFn: getInfinitePosts,
+    getNextPageParam: (lastPage) => {
+      // If there's no data, there are no more pages.
+      if (lastPage && lastPage.documents.length === 0) {
+        return null;
+      }
+
+      // Use the $id of the last document as the cursor.
+      const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
+      return lastId;
+    },
+  });
+};
+
+export const useSearchPosts = (searchTerm: string) => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.SEARCH_POSTS, searchTerm],
+    queryFn: () => searchPosts(searchTerm),
+    enabled: !!searchTerm,
   });
 };
